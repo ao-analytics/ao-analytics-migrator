72cf8a45aa9104e8b46e7fbd01f3e5eb6335af47
veqox <sonnleitner.luki@gmail.com>
2024-02-15 15:08:03 +0100

    bump: update dependencies fix: download script not overriding the old versions

--- download.sh
+++ download.sh
@@ -2,6 +2,6 @@
 
 mkdir -p data
 
-curl https://raw.githubusercontent.com/ao-data/ao-bin-dumps/master/formatted/world.json >> data/locations.json
-curl https://raw.githubusercontent.com/ao-data/ao-bin-dumps/master/items.json >> data/items.json
-curl https://raw.githubusercontent.com/ao-data/ao-bin-dumps/master/formatted/items.json >> data/localizations.json
+curl https://raw.githubusercontent.com/ao-data/ao-bin-dumps/master/formatted/world.json > data/locations.json
+curl https://raw.githubusercontent.com/ao-data/ao-bin-dumps/master/items.json > data/items.json
+curl https://raw.githubusercontent.com/ao-data/ao-bin-dumps/master/formatted/items.json > data/localizations.json
