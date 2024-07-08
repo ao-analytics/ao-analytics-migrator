ef4b4d98ab513604fcbcb2712b738ac5049bee38
veqox <sonnleitner.luki@gmail.com>
2024-07-08 20:03:18 +0200

    feat: store complete item data as jsonb

--- src/utils/config.rs
+++ src/utils/config.rs
@@ -2,8 +2,10 @@ use config::Config;
 
 #[derive(Debug, Config)]
 pub struct Config {
+    pub items_path: String,
     pub locations_path: String,
     pub localizations_path: String,
+    pub items_url: String,
     pub locations_url: String,
     pub localizations_url: String,
     pub database_url: String,
--- src/utils/json.rs
+++ src/utils/json.rs
@@ -33,6 +33,97 @@ pub fn get_locations_from_file(path: &str) -> Option<Vec<json::location::Locatio
     }
 }
 
+pub fn get_items_from_file(path: &str) -> Option<Vec<json::item::Item>> {
+    match std::fs::read_to_string(path) {
+        Ok(content) => match serde_json::from_str::<json::item::Root>(&content) {
+            Ok(items) => {
+                let mut items_vec: Vec<json::item::Item> = Vec::new();
+                items_vec.push(items.items.hideout_item);
+                items
+                    .items
+                    .tracking_item
+                    .into_iter()
+                    .for_each(|item| items_vec.push(item));
+                items
+                    .items
+                    .farmable_item
+                    .into_iter()
+                    .for_each(|item| items_vec.push(item));
+                items
+                    .items
+                    .simple_item
+                    .into_iter()
+                    .for_each(|item| items_vec.push(item));
+                items
+                    .items
+                    .consumable_item
+                    .into_iter()
+                    .for_each(|item| items_vec.push(item));
+                items
+                    .items
+                    .consumable_from_inventory_item
+                    .into_iter()
+                    .for_each(|item| items_vec.push(item));
+                items
+                    .items
+                    .equipment_item
+                    .into_iter()
+                    .for_each(|item| items_vec.push(item));
+                items
+                    .items
+                    .weapon
+                    .into_iter()
+                    .for_each(|item| items_vec.push(item));
+                items
+                    .items
+                    .mount
+                    .into_iter()
+                    .for_each(|item| items_vec.push(item));
+                items
+                    .items
+                    .furniture_item
+                    .into_iter()
+                    .for_each(|item| items_vec.push(item));
+                items
+                    .items
+                    .mount_skin
+                    .into_iter()
+                    .for_each(|item| items_vec.push(item));
+                items
+                    .items
+                    .journal_item
+                    .into_iter()
+                    .for_each(|item| items_vec.push(item));
+                items
+                    .items
+                    .labourer_contract
+                    .into_iter()
+                    .for_each(|item| items_vec.push(item));
+                items
+                    .items
+                    .transformation_weapon
+                    .into_iter()
+                    .for_each(|item| items_vec.push(item));
+                items
+                    .items
+                    .crystal_league_item
+                    .into_iter()
+                    .for_each(|item| items_vec.push(item));
+                items_vec.push(items.items.kill_trophy);
+                Some(items_vec)
+            }
+            Err(e) => {
+                warn!("Error parsing items file: {}", e);
+                None
+            }
+        },
+        Err(e) => {
+            warn!("Error reading items file: {}", e);
+            None
+        }
+    }
+}
+
 pub async fn save_file_to_disk(path: &str, content: &str) {
     match std::fs::write(path, content) {
         Ok(_) => info!("File saved to disk: {}", path),
