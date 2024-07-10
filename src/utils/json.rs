4c6964fdfc2225cf9e64ef38ea8bc4abab4d5082
veqox <sonnleitner.luki@gmail.com>
2024-07-10 21:52:56 +0200

    fix: add missing trash items

--- src/utils/json.rs
+++ src/utils/json.rs
@@ -46,6 +46,11 @@ pub fn get_items_from_file(path: &str) -> Option<Vec<json::item::Item>> {
                     .for_each(|item| items_vec.push(item));
                 items
                     .items
+                    .trash_item
+                    .into_iter()
+                    .for_each(|item| items_vec.push(item));
+                items
+                    .items
                     .farmable_item
                     .into_iter()
                     .for_each(|item| items_vec.push(item));
